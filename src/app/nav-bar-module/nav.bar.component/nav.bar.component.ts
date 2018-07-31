32e36fd1f6a36842caa97f8ca04c3d0f1ed4df04
costia097 <adaw36909@gmail.com>
2018-07-31 17:38:15 +0300

    redesign main page to bootstrap template

--- src/app/nav-bar-module/nav.bar.component/nav.bar.component.ts
+++ src/app/nav-bar-module/nav.bar.component/nav.bar.component.ts
@@ -1,52 +1,85 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Router} from '@angular/router';
 import {LoginData} from '../../login.module/LoginData';
 
 @Component({
   selector: 'app-navbar',
   template: `
-    <div class="nav-bar">
-      <div class="container">
-        <div class="row">
-          
-          <div *ngIf="userInfo == null" class="col-3">
-            <nav>
-              <a routerLink="/login" routerLinkActive="active">Login</a>
-            </nav>
-          </div>
-          
-          <div class="col-3">
-            <nav>
-              <a routerLink="/content" routerLinkActive="active">Content</a>
-            </nav>
-          </div>
-          
-          <div *ngIf="userInfo == null" class="col-3">
-            <nav>
-              <a routerLink="/signUp" routerLinkActive="active">SignUp</a>
-            </nav>
-          </div>
+   <body>
+   <div class="wrapper">
+     <!-- Sidebar  -->
+     <nav id="sidebar" [ngClass]="{'active':isHidden}">
+       <div class="sidebar-header">
+         <h3>Watcher</h3>
+         <img class="logo-img" src="https://seeklogo.com/images/J/Java-logo-6BBEB11CBA-seeklogo.com.png"/>
+         
+       </div>
 
-          <div *ngIf="userInfo != null" class="col-3">
-            <a href="/" (click)="logOut()" >Log Out</a>
-          </div>
-          
-        </div>
-      </div>
-    </div>
-    
-    <div *ngIf="userInfo!=null">
-      <p>Hello, {{userInfo.userName}}</p>
-    </div>
+       <ul class="list-unstyled components">
+         <p>Watcher features</p>
+         <li class="active">
+           <a href="#" data-toggle="collapse" aria-expanded="false" >Home</a>
+         </li>
+         <li>
+           <a href="#">About</a>
+         </li>
+         <li>
+           <a href="#" data-toggle="collapse" aria-expanded="false">Pages</a>
+         </li>
+       </ul>
+     </nav>
 
-    <router-outlet></router-outlet>
+     <!-- Page Content  -->
+     <div id="content">
+
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <div class="container-fluid">
+
+           <button type="button" id="sidebarCollapse" class="btn btn-info" (click)="hideSideBar()">
+             <i class="fas fa-align-left"></i>
+             <span>Toggle Sidebar</span>
+           </button>
+           
+           <div>
+             <img class="logo-img-nav-bar" src="http://www.sepiscreens.it/img/svg/logo.svg"/>
+           </div>
+           
+           <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+             <i class="fas fa-align-justify"></i>
+           </button>
+
+           <div class="collapse navbar-collapse" id="navbarSupportedContent">
+             <ul class="nav navbar-nav ml-auto">
+               <div class="row navbar-content">
+                 <li class="nav-item ">
+                   <a class="nav-link" routerLink="/content" routerLinkActive="active">Content</a>
+                 </li>
+               </div>
+               
+               <li class="nav-item ">
+                 <a class="nav-link" routerLink="/login" routerLinkActive="active">Login</a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="#">Logout</a>
+               </li>
+             </ul>
+           </div>
+         </div>
+       </nav>
+
+       <router-outlet></router-outlet>
+     </div>
+   </div>
+   </body>
   `,
   styleUrls: ['nav.bar.component.css']
 })
 export class NavBarComponent implements OnInit {
   userInfo: LoginData;
+  isHidden: boolean = false;
 
+  hideSideBar() {
+    this.isHidden = !this.isHidden;
+  }
 
   ngOnInit(): void {
     let userInfoText = localStorage.getItem('currentUser');
--- src/app/nav-bar-module/nav.bar.routing.module.ts
+++ src/app/nav-bar-module/nav.bar.routing.module.ts
@@ -4,7 +4,6 @@ import {LoginFormComponent} from '../login.module/login.component/login.form.com
 import {ContentComponent} from '../content.module/content.component/content.component';
 import {ContentInfoComponent} from '../content.module/content.info.component/content.info.component';
 import {SignUpComponent} from '../content.module/signUp.component/signUp.component';
-import {AppComponent} from '../app.component';
 
 @NgModule({
   imports: [
